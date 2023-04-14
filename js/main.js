0d46f04bb5c1a01e7da7d4345156817c1a47443c
ann654 <121104592+ann654@users.noreply.github.com>
2023-04-14 12:09:46 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+// const score = 15%2
+// console.log(score);
+
+// let num1 = 8
+// num1++
+// num1++
+// console.log(num1);
+
+// let x = 5
+// let y = 10
+// console.log(x*=y);
+// console.log(x/=y);
+
+
+// const passLength = 12
+
+
+// if (passLength > 10) {
+// 	console.log(`Masz bardzo dobre hasło!`);
+// }
+// else if (passLength >= 5 && passLength <=10 ) {
+// 	console.log(`Masz dobre hasło!`);
+// }
+// else {console.log(`Masz słabe hasło!`)}
+
+// function addNumbers(x, y) {
+// 	return x + y
+// }
+// addNumbers(5, 8)
+
+// #3
+
+// }
+const numbers = [1, 2, 3, 4, 5, 6]
+for(let i = 0; i < numbers.length; i++) {
+	console.log(numbers[i]);
+}
+function test(arg1, arg2, arg3){
+	console.log(arg1 + arg2 * arg3);
+}
+test(1, 2, 3)
+if(numbers.length > 5){
+	console.log(`TRUE`);
+}
+	else {
+		console.log(`FALSE`);
+	}
